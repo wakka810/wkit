4579c157d935d1513d92d20c1affec9cec4d565f
Wakka <rw24013y@st.omu.ac.jp>
2026-01-09 05:29:34 +0000

    add mini.h

---
+++ mini.h
@@ -0,0 +1,39 @@
+#ifndef MINI_H
+#define MINI_H
+
+#include <assert.h>
+#include <complex.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fenv.h>
+#include <float.h>
+#include <inttypes.h>
+#include <iso646.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdalign.h>
+#include <stdarg.h>
+#include <stdatomic.h>
+#include <stdbit.h>
+#include <stdbool.h>
+#include <stdckdint.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+#include <string.h>
+#include <tgmath.h>
+#include <threads.h>
+#include <time.h>
+#include <uchar.h>
+#include <wchar.h>
+#include <wctype.h>
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#endif
